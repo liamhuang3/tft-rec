93c43e27be118d2f465ecef4eb47eab1c55dbf2d
Liam Huang <liamh@Liams-MacBook-Pro.local>
2023-10-29 15:46:05 -0400

    riot api progress

--- src/SelectorCard.tsx
+++ src/SelectorCard.tsx
@@ -3,6 +3,7 @@ import { Grid, Card, CardContent, Typography, Button, ToggleButton, ToggleButton
 import DropdownSelector from './DropdownSelector'
 import RecommendationCard from './RecommendationCard';
 import { Option } from './DropdownSelector';
+import { RiotAPI, RiotAPITypes, PlatformId } from '@fightmegg/riot-api'
 
 const SelectorCard: React.FC = () => {
     const [showRecommendation, setShowRecommendation] = useState(false);
@@ -43,6 +44,14 @@ const SelectorCard: React.FC = () => {
         }
       };
     const handleRecommendClick = () => {
+        (async () => {
+            const rAPI = new RiotAPI('');
+        
+            const summoner = await rAPI.summoner.getBySummonerName({
+                region: PlatformId.NA1,
+                summonerName: "chillycakes",
+              });
+        })()
         // When the "Recommend" button is clicked, set showRecommendation to true
         setShowRecommendation(true);
     };
